2876f718db401693a2571c00d957a59a993e9494
MelodyLLL <d_o_double_g@163.com>
2023-05-16 15:39:14 +0800

    fix: 包裹自定义组件

--- docs/.vuepress/CustomLayout.vue
+++ docs/.vuepress/CustomLayout.vue
@@ -1,20 +1,23 @@
 <script setup lang="ts">
-import Layout from "@vuepress/theme-default/lib/client/layouts/Layout.vue";
+import Layout from '@vuepress/theme-default/lib/client/layouts/Layout.vue';
 // import HomeContent from "@theme/HomeContent.vue";
-import HomeFeatures from "./CustomHomeFeatures.vue";
-import HomeFooter from "@theme/HomeFooter.vue";
-import HomeHero from "./CustomHomeHero.vue";
+import HomeFeatures from './CustomHomeFeatures.vue';
+import HomeFooter from '@theme/HomeFooter.vue';
+import HomeHero from './CustomHomeHero.vue';
+import { ClientOnly } from '@vuepress/client';
 </script>
 
 <template>
-  <Layout>
-    <template #page>
-      <main class="home">
-        <HomeHero />
-        <HomeFeatures />
-        <!-- <HomeContent /> -->
-        <HomeFooter />
-      </main>
-    </template>
-  </Layout>
+	<ClientOnly>
+		<Layout>
+			<template #page>
+				<main class="home">
+					<HomeHero />
+					<HomeFeatures />
+					<!-- <HomeContent /> -->
+					<HomeFooter />
+				</main>
+			</template>
+		</Layout>
+	</ClientOnly>
 </template>

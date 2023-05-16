a503cd65a5171896f0951aeb53c51427367bf709
MelodyLLL <d_o_double_g@163.com>
2023-05-16 18:16:14 +0800

    fix: 修复主题继承方式7

--- docs/.vuepress/components/CustomHomeFeatures.vue
+++ docs/.vuepress/components/CustomHomeFeatures.vue
@@ -1,128 +1,127 @@
 <script setup>
+import { ClientOnly } from '@vuepress/client';
 
 const props = defineProps({});
 </script>
 <!-- horizontal -->
 <template>
-	  <!-- <h4>Horizontal Flip</h4> -->
-  	<p>
-  	  <div class="flip_wrapper">
-    		<div class="flip">
-    			<div
-    				class="front"
-    				style="
-    					background-image: url(https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb);
-    				"
-    			>
-    				<h1 class="text-shadow">MOUNTAIN</h1>
-    			</div>
-    			<div class="back">
-    				<h2>Angular</h2>
-    				<p>
-    					Good tools make application development quicker and easier to maintain
-    					than if you did everything by hand..
-    				</p>
-    			</div>
-    		</div>
-    		<div class="flip">
-    			<div
-    				class="front"
-    				style="
-    					background-image: url(https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb);
-    				"
-    			>
-    				<h1 class="text-shadow">LAKE</h1>
-    			</div>
-    			<div class="back">
-    				<h2>Angular</h2>
-    				<p>
-    					Good tools make application development quicker and easier to maintain
-    					than if you did everything by hand..
-    				</p>
-    			</div>
-    		</div>
-    		<div class="flip">
-    			<div
-    				class="front"
-    				style="
-    					background-image: url(https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb);
-    				"
-    			>
-    				<h1 class="text-shadow">OCEAN</h1>
-    			</div>
-    			<div class="back">
-    				<h2>Angular</h2>
-    				<p>
-    					Good tools make application development quicker and easier to maintain
-    					than if you did everything by hand..
-    				</p>
-    			</div>
-    		</div>
-    	</div>
-    
-    	<br />
-    	<br />
-    
-    	<!-- vertical -->
-    
-    	<!-- <h4>Vertical Flip</h4> -->
-    	<div class="flip_wrapper">
-    		<div class="flip flip-vertical">
-    			<div
-    				class="front"
-    				style="
-    					background-image: url(https://images.pexels.com/photos/38136/pexels-photo-38136.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb);
-    				"
-    			>
-    				<h1 class="text-shadow">FOREST</h1>
-    			</div>
-    			<div class="back">
-    				<h2>Angular</h2>
-    				<p>
-    					Good tools make application development quicker and easier to maintain
-    					than if you did everything by hand..
-    				</p>
-    			</div>
-    		</div>
-    		<div class="flip flip-vertical">
-    			<div
-    				class="front"
-    				style="
-    					background-image: url(https://images.pexels.com/photos/34950/pexels-photo.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb);
-    				"
-    			>
-    				<h1 class="text-shadow">TRACK</h1>
-    			</div>
-    			<div class="back">
-    				<h2>Angular</h2>
-    				<p>
-    					Good tools make application development quicker and easier to maintain
-    					than if you did everything by hand..
-    				</p>
-    			</div>
-    		</div>
-    		<div class="flip flip-vertical">
-    			<div
-    				class="front"
-    				style="
-    					background-image: url(https://images.pexels.com/photos/34546/sunset-lake-landscape-summer.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb);
-    				"
-    			>
-    				<h1 class="text-shadow">SWAMP</h1>
-    			</div>
-    			<div class="back">
-    				<h2>Angular</h2>
-    				<p>
-    					Good tools make application development quicker and easier to maintain
-    					than if you did everything by hand..
-    				</p>
-    			</div>
-    		</div>
-    	</div>
-  	</p>
+	<!-- <h4>Horizontal Flip</h4> -->
+	<div class="flip_wrapper">
+		<div class="flip">
+			<div
+				class="front"
+				style="
+					background-image: url(https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb);
+				"
+			>
+				<h1 class="text-shadow">MOUNTAIN</h1>
+			</div>
+			<div class="back">
+				<h2>Angular</h2>
+				<p>
+					Good tools make application development quicker and easier to maintain
+					than if you did everything by hand..
+				</p>
+			</div>
+		</div>
+		<div class="flip">
+			<div
+				class="front"
+				style="
+					background-image: url(https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb);
+				"
+			>
+				<h1 class="text-shadow">LAKE</h1>
+			</div>
+			<div class="back">
+				<h2>Angular</h2>
+				<p>
+					Good tools make application development quicker and easier to maintain
+					than if you did everything by hand..
+				</p>
+			</div>
+		</div>
+		<div class="flip">
+			<div
+				class="front"
+				style="
+					background-image: url(https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb);
+				"
+			>
+				<h1 class="text-shadow">OCEAN</h1>
+			</div>
+			<div class="back">
+				<h2>Angular</h2>
+				<p>
+					Good tools make application development quicker and easier to maintain
+					than if you did everything by hand..
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<br />
+	<br />
+
+	<!-- vertical -->
+
+	<!-- <h4>Vertical Flip</h4> -->
+	<div class="flip_wrapper">
+		<div class="flip flip-vertical">
+			<div
+				class="front"
+				style="
+					background-image: url(https://images.pexels.com/photos/38136/pexels-photo-38136.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb);
+				"
+			>
+				<h1 class="text-shadow">FOREST</h1>
+			</div>
+			<div class="back">
+				<h2>Angular</h2>
+				<p>
+					Good tools make application development quicker and easier to maintain
+					than if you did everything by hand..
+				</p>
+			</div>
+		</div>
+		<div class="flip flip-vertical">
+			<div
+				class="front"
+				style="
+					background-image: url(https://images.pexels.com/photos/34950/pexels-photo.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb);
+				"
+			>
+				<h1 class="text-shadow">TRACK</h1>
+			</div>
+			<div class="back">
+				<h2>Angular</h2>
+				<p>
+					Good tools make application development quicker and easier to maintain
+					than if you did everything by hand..
+				</p>
+			</div>
+		</div>
+		<div class="flip flip-vertical">
+			<div
+				class="front"
+				style="
+					background-image: url(https://images.pexels.com/photos/34546/sunset-lake-landscape-summer.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb);
+				"
+			>
+				<h1 class="text-shadow">SWAMP</h1>
+			</div>
+			<div class="back">
+				<h2>Angular</h2>
+				<p>
+					Good tools make application development quicker and easier to maintain
+					than if you did everything by hand..
+				</p>
+			</div>
+		</div>
+	</div>
 </template>
 <style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
+// @import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
 // * {
 // 	box-sizing: border-box;
 // 	font-weight: normal;
@@ -153,22 +152,26 @@ h1 {
 			display: block;
 			transition-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.275);
 			-ms-transition-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.275);
-      -moz-transition-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.275);
-      -webkit-transition-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.275);
-      -o-transition-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.275);
+			-moz-transition-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.275);
+			-webkit-transition-timing-function: cubic-bezier(
+				0.175,
+				0.885,
+				0.32,
+				1.275
+			);
+			-o-transition-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.275);
 
 			transition-duration: 1.5s;
-      -ms-transition-duration: 1.5s;
-      -moz-transition-duration: 1.5s;
-      -webkit-transition-duration: 1.5s;
-      -o-transition-duration: 1.5s;
+			-ms-transition-duration: 1.5s;
+			-moz-transition-duration: 1.5s;
+			-webkit-transition-duration: 1.5s;
+			-o-transition-duration: 1.5s;
 
 			transition-property: transform, opacity;
 			-ms-transition-property: transform, opacity;
 			-moz-transition-property: transform, opacity;
-      -webkit-transition-property: transform, opacity;
+			-webkit-transition-property: transform, opacity;
 			-o-transition-property: transform, opacity;
-
 		}
 		> .front {
 			transform: rotateY(0deg);

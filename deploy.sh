c6c83d55f1026548aacced0d6e2d65635a77aec7
MelodyLLL <d_o_double_g@163.com>
2023-06-12 00:03:08 +0800

    feat: 修改打包命令

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 # 执行 VuePress 打包命令，将生成的 dist 目录输出到 temp 目录
 pnpm build
 
+# 创建temp目录
 mkdir temp
 
 # 进入 temp 目录
@@ -17,12 +18,11 @@ git add .
 # 提交并推送到 GitHub 的远程仓库
 git commit -m "Deploy dist"
 
-# 将远程仓库的 URL 替换为你自己的 GitHub 仓库 URL
 git remote add origin https://github.com/MelodyLLL/MelodyLLL_BLOG.git
 
 git checkout -b gh-pages
 
-git pull origin gh-pages --allow-unrelated-histories
+# git pull origin gh-pages --allow-unrelated-histories
 # 强制推送到远程仓库的 gh-pages 分支
 git push -f origin gh-pages
 
